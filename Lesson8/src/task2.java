84fbed676a1f919e5abda022eeab5bd0591594cd
Alex <deaddemon39@list.ru>
2023-05-02 00:30:28 +0300

    Разбор задач готов

--- Lesson8/src/task2.java
+++ Lesson8/src/task2.java
@@ -11,7 +11,7 @@ public class task2 {
         //Методы и их логику продумайте сами
 
         num = inputNumber();
-        checkNum();
+        checkNum(); // МОЖНО БЫЛО ОБОЙТИСЬ ОДНОЙ СТРОКОЙ!!!
 
 
 
@@ -22,10 +22,10 @@ public class task2 {
         System.out.println("Введите целое число: ");
         return new Scanner(System.in).nextInt();
     }
-        private static void checkNum() {
+        private static void checkNum() {   // System.out.println(num % 2 == 0 ? true : false) ТЕРНАРНЫЙ СПОСОБ ЗАПИСИ
             if (num % 2 == 0) {
-                System.out.println(a);
-            }
+                System.out.println(a);  // System.out.println(num % 2 == 0)   УПРОЩЕННЫЙ СПОСОБ. вернет либо true либо false
+            }                           // System.out.println(inputNumber() % 2 == 0);  // МЕТОД В МЕТОДЕ!
             else {
                 a = false;
                 System.out.println(a);
